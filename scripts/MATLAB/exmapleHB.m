396050c58878d4f77a5e9f54f758ed970b0457c9
mrtnzrma <csg1c12@gmail.com>
2025-08-06 22:40:56 -0500

    Update exampleHB.m

--- scripts/MATLAB/exmapleHB.m
+++ scripts/MATLAB/exmapleHB.m
@@ -5,13 +5,13 @@ addpath("path_to_HierarchicalConsensus/");       % see README.md for details
 % Example usage of the HCE method on Hierarchical Benchmark
 
 % Generate a Hierarchical Benchmark model
-p0 = 0.05; % background probability of edges
-p1 = 0.3; % Probability of edges in the first level (second renormalized level)
-p2 = 0.4; % Probability of edges in the second level (first renormalized level)
+p0 = 0.05;  % background probability of edges
+p1 = 0.2;   % Probability of edges in the first level (second renormalized level)
+p2 = 0.3;   % Probability of edges in the second level (first renormalized level)
 p3 = 1 - p0 - p1 - p2; % Probability of edges in the fourth level (zeroth renormalized level)
 
 [A, Sgtruth] = hierarchicalBenchmark(1000, [p0, p1, p2, p3]);
-S = eventSamples(A, 1000);
+S = eventSamples(A, 100);
 [Sc, Tree] = hierarchicalConsensus(S);
 
 % Transform Tree to a linkage matrix (nTree)
@@ -28,5 +28,5 @@ if ~isempty(Tree)                           % Check if Tree is not empty (very r
 
     % Second renormalization level
     [labels, ~] = findHCELevel(nTree, [], 2);
-    fprintf("Second renormalization level: %f\n", AMI(labels, Sgtruth(:, end) - 2));
+    fprintf("Second renormalization level: %f\n", AMI(labels, Sgtruth(:, end - 2)));
 end
